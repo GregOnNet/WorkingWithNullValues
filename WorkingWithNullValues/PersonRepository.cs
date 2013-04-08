f56b504daf30c73fd00bc5bb9ae1d90473e04a01
Gregor Woiwode <gregor.woiwode@gmail.com>
2013-04-08 19:11:23 +0200

    Adding repository to find a person in a list. Showing that null values can be created by empty search results.

---
+++ WorkingWithNullValues/PersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace WorkingWithNullValues
+{
+  public class PersonRepository
+  {
+    List<Person> _persons = new List<Person>();
+
+    public Person FindPerson(string name)
+    {
+      foreach (var person in _persons)
+        if (person.Name == name)
+          return person;
+      return null;
+    }
+  }
+}
