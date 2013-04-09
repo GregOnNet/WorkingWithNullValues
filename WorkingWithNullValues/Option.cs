ff39272d04ae2a23da7e697c89a2fa050240a22a
Gregor Woiwode <gregor.woiwode@gmail.com>
2013-04-09 14:53:32 +0200

    Introduced the option type.

---
+++ WorkingWithNullValues/Option.cs
@@ -0,0 +1,16 @@
+﻿namespace WorkingWithNullValues
+{
+  public interface Option<T> { }
+
+  public class None<T> : Option<T> { }
+
+  public class Some<T> : Option<T>
+  {
+    public T Value { get; set; }
+    
+    public Some( T value)
+    {
+      Value = value;
+    }
+  }
+}
---
+++ WorkingWithNullValues/OptionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WorkingWithNullValues
+{
+  public static class OptionExtensions
+  {
+     public static S Match<T, S>(
+       this Option<T> value,
+       Func<S> ifNone,
+       Func<T, S> ifSome)
+     {
+       if (value is Some<T>)
+         return ifSome(((Some<T>) value).Value);
+       return ifNone();
+     }
+  }
+}
