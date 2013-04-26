d668e7b494214eddf311e5ccdfbb37d464bd339e
Gregor Woiwode <gregor.woiwode@gmail.com>
2013-04-26 23:12:33 +0200

    Added FirstSteps to Repository.

---
+++ FirstSteps/CustomExceptions/PersonNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace FirstSteps.CustomExceptions
+{
+  public class PersonNotFoundException : Exception {}
+}
---
+++ FirstSteps/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FirstSteps.Repositories;
+
+namespace FirstSteps
+{
+  public class Program
+  {
+    static void Main()
+    {
+      var peter = "peter";
+      var person = new PersonRepository();
+
+      if (person.IsOlderThan12(peter))
+        Console.WriteLine(
+          "{0} is older than 12.", peter);
+      Console.Read();
+    }
+  }
+}
